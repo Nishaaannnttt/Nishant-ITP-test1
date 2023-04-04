603d42697c7886b4454e49d3b316e17692b3d5c5
Nishant <bhusalnishant93@gmail.com>
2023-04-04 09:24:16 -0400

    made the first change in part 2 qn

---
+++ part2.py
@@ -0,0 +1,7 @@
+#   MARKING GLOBAL VARIABLES 
+RED="red"
+BLUE="blue"
+YELLOW="yellow"
+
+color1=input("enter the  primary color in lowercase letter :")
+color2=input("enter the secondary color in lowercase letter :")
