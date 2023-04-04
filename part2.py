3f8c7c17f97056c6ab8e7e2ef741d443d5cc9be4
Nishant <bhusalnishant93@gmail.com>
2023-04-04 09:52:02 -0400

    made second change in qn 2

--- part2.py
+++ part2.py
@@ -5,3 +5,36 @@ YELLOW="yellow"
 
 color1=input("enter the  primary color in lowercase letter :")
 color2=input("enter the secondary color in lowercase letter :")
+
+#    CHECKING THE COLORS 
+if color1!=RED and color1!=BLUE and color1!=YELLOW :
+    print("Error : Invalid Color1")
+
+elif color2!=RED and color2!=BLUE and color2!=YELLOW:
+    print("Error : Invalid Color2")
+    
+elif color1 == color2:
+    print("Error : The two colors you entered are same ")
+    
+else:
+    #   MIXING THE COLORS 
+    if color1==RED:
+        if color2==BLUE:
+            print("PURPLE")
+        elif color2==YELLOW:
+                print("ORANGE")
+    
+    if color1==BLUE:
+        if color2==RED:
+            print("PURPLE")
+        elif color2==YELLOW:
+            print('GREEN')
+            
+    if color1==YELLOW:
+        if color2==RED:
+            print("ORANGE")
+        elif color2==BLUE:
+            print("GREEN")
+    
+
+    
